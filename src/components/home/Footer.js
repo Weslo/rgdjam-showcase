7e4bf7f7fed47be23c23d7667967d5c2025142cb
Weslo <wesley.rockholz@gmail.com>
2017-09-16 19:47:05 -0400

    Add some style to the header and footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -4,15 +4,15 @@ const Component = require('../core/Component');
 class Footer extends Component {
 
     view() {
-        return('.footer', [
-            m('.about', [
+        return(m('.footer', [
+            m('.about col-2', [
                 m('h4', "About"),
                 m('p', "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus dolor sed accumsan placerat. Integer orci sem, eleifend a lorem a, scelerisque viverra augue. Fusce sagittis, elit sagittis tristique lacinia, orci purus mattis erat, et accumsan dolor ipsum ac risus. Donec quis urna aliquet libero malesuada dapibus eu vel mi. Nulla congue euismod sodales. Nulla non commodo dolor. Mauris ut ipsum cursus, malesuada mi ac, lacinia nisi. Etiam interdum sapien quis libero imperdiet consequat. In congue non leo eu tristique.")
             ]),
-            m('.social', [
+            m('.social col-2', [
                 m('h4', "Join Us")
             ])
-        ]);
+        ]));
     }
 }
 
