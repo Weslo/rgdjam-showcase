97270957a5f6c68a63a705be309576254f2bedd4
Weslo <wesley.rockholz@gmail.com>
2017-09-20 20:10:59 -0400

    Add some hover styles to game cards

--- src/components/home/GameCard.js
+++ src/components/home/GameCard.js
@@ -4,11 +4,13 @@ const Component = require('../core/Component');
 class GameCard extends Component {
 
     view() {
-        return m('.game-card col-2', [
-            m('img[src="games/test/banner.png"]'),
-            m('.title', "Test Game"),
-            m('.description', "This is an example of an awesome game made right here in Rochester!")
-        ]);
+        return m('.game-card col-2',
+            m('.container', [
+                m('img[src="games/test/banner.png"]'),
+                m('.title', "Test Game"),
+                m('.description', "This is an example of an awesome game made right here in Rochester!")
+            ])
+        );
     }
 }
 
