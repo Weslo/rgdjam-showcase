4e8bbf291248b35e95359d3ba1e681eabaf84a47
Weslo <wesley.rockholz@gmail.com>
2017-09-29 23:12:37 -0400

    Update header and footer style

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -8,12 +8,12 @@ class Footer extends Component {
         return m('.footer', [
             m('.row', [
                 m('.about col-2', [
-                    m('h4', "About"),
+                    m('h2', "About"),
                     m('p', "ROC Game Dev is monthly meetup of video game developers that anyone can attend. Come hang out, learn how to make games, and join the community."),
                     m('p', "The Rochester Mini-Game Suite is a ongoing collection of small games all about Rochester. We started making these over a weekend and now you can play them! If you have a game idea and want to get your game up here, come to a meetup and let's do it!")
                 ]),
                 m('.social col-2', [
-                    m('h4', "Join Us"),
+                    m('h2', "Join Us"),
                     m('ul', [
                         m(SocialMediaLinkIcon, {
                             link: "https://discordapp.com/invite/5Aafv5t",
